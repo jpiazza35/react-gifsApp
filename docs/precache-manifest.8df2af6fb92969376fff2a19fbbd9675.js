591cd61798f48ca5ba56a2b8264ba7219c462aab
jose <piazzajignacio@gmail.com>
2020-09-06 15:57:56 -0300

    docs agregado

---
+++ docs/precache-manifest.8df2af6fb92969376fff2a19fbbd9675.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f51d8deacda46af81317dbe343a67b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2fc8c330a9b57666896",
+    "url": "/static/css/main.a2e8e213.chunk.css"
+  },
+  {
+    "revision": "e610eb1881ba5d357588",
+    "url": "/static/js/2.6c4bc377.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6c4bc377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2fc8c330a9b57666896",
+    "url": "/static/js/main.70b5b6fd.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
